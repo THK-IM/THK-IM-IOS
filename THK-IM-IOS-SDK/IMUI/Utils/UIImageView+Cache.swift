164fc6b6b535fb82f8f155b2e76b30b0f677cc09
vizoss <think220216@gmail.com>
2024-08-08 21:35:31 +0800

    fix bug

--- THK-IM-IOS-SDK/IMUI/Utils/UIImageView+Cache.swift
+++ THK-IM-IOS-SDK/IMUI/Utils/UIImageView+Cache.swift
@@ -13,14 +13,16 @@ public extension UIImageView {
     
     func renderImageByPathWithCorner(path: String, radius: CGFloat, placeHolderImage: UIImage? = nil) {
         self.layer.cornerRadius = radius
-        self.clipsToBounds = true
         self.kf.setImage(with: LocalFileImageDataProvider(fileURL: URL(fileURLWithPath: path)), placeholder: placeHolderImage)
     }
     
     func renderImageByUrlWithCorner(url: String, radius: CGFloat, placeHolderImage: UIImage? = nil) {
         self.layer.cornerRadius = radius
-        self.clipsToBounds = true
         self.kf.setImage(with: URL(string: url), placeholder: placeHolderImage)
+    }
+    
+    func renderImageByUrl(url: String, placeHolderImage: UIImage? = nil, _ completeHandler: ((Result<RetrieveImageResult, KingfisherError>) -> Void)? = nil) {
+        self.kf.setImage(with: URL(string: url), placeholder: placeHolderImage, completionHandler: completeHandler)
     }
     
     func renderImageByPath(path: String, placeHolderImage: UIImage? = nil) {
@@ -29,7 +31,6 @@ public extension UIImageView {
     
     func renderImageByPath(path: String, radius: CGFloat, placeHolderImage: UIImage? = nil) {
         self.layer.cornerRadius = radius
-        self.clipsToBounds = true
         self.kf.setImage(with: LocalFileImageDataProvider(fileURL: URL(fileURLWithPath: path)), placeholder: placeHolderImage)
     }
     
