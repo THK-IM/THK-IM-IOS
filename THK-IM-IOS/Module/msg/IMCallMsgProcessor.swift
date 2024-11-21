a75390f7c8370f327c2a45f32ebb02b7b8a8abbe
vizoss <think220216@gmail.com>
2024-11-21 19:28:11 +0800

    clean code

--- THK-IM-IOS/Module/msg/IMCallMsgProcessor.swift
+++ THK-IM-IOS/Module/msg/IMCallMsgProcessor.swift
@@ -14,14 +14,14 @@ class IMCallMsgProcessor: IMBaseMsgProcessor {
     }
 
     override func msgDesc(msg: Message) -> String {
-        guard let d = msg.content?.data(using: .utf8) else { return "" }
+        guard let d = msg.content?.data(using: .utf8) else { return "[语音通话]" }
         guard let callMsg = try? JSONDecoder().decode(IMCallMsg.self, from: d) else {
-            return ""
+            return "[语音通话]"
         }
         if callMsg.roomMode == RoomMode.Audio.rawValue {
-            return "语音通话"
+            return "[语音通话]"
         } else {
-            return "视频通话"
+            return "[视频通话]"
         }
     }
 }
